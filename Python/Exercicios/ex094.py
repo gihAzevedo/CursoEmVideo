5077db863fb979fb852cf074cbcdbcb479cc8cbe
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-07-24 18:49:56 -0300

    Update ex094.py
    Finalização da resolução do exercício 94.

--- Python/Exercicios/ex094.py
+++ Python/Exercicios/ex094.py
@@ -26,10 +26,19 @@ while True:
     if resp == 'N':
         break
 print('-=' * 30)
-print(f'Ao todo temos {len(galera)} pessoas cadastradas.')
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
 media = soma / len(galera)
-print(f'A média de idade é de {media:5.2f} anos.')
-print('As Mulheres cadastradas foram ', end='')
+print(f'B) A média de idade é de {media:5.2f} anos.')
+print('C) As Mulheres cadastradas foram ', end='')
 for p in galera:
-    if p[]
-
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]} ', end='')
+print()
+print('D) Lista de pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('     ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
