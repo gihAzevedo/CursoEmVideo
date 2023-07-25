0160a9fcabc2f6387b60f14cd2c26e0eba9c69ac
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-07-25 16:32:03 -0300

    Create aula20.py
    Inicialização da aula 20 sobre Funções

---
+++ Python/pythonteste/aula20.py
@@ -0,0 +1,28 @@
+def contador(* num): #Colocar asteristico possibilita colocar vários parametros e colocar dentro de tuplas
+    print(num)
+
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
+
+
+
+
+PAREI: 32:43
+
+
+
+
+'''
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+    print('-' * 15)
+
+
+# Programa Principal
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)
+'''
