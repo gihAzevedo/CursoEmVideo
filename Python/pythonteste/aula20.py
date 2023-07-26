5d8e83bc8b6f4b46f066eb30d3bd16f64e578662
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-07-26 07:46:29 -0300

    Update aula20.py
    Término da aula 20.

--- Python/pythonteste/aula20.py
+++ Python/pythonteste/aula20.py
@@ -1,19 +1,24 @@
+def dobra(lst):
+    pos = 0
+    while pos < len(lst):
+        lst[pos] *= 2
+        pos += 1
+
+valores = [6, 3, 9, 1, 0, 2]
+dobra(valores)
+print(valores)
+
+'''
 def contador(* num): #Colocar asteristico possibilita colocar vários parametros e colocar dentro de tuplas
-    print(num)
+    tam = len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam} números')
 
 contador(2, 1, 7)
 contador(8, 0)
 contador(4, 4, 7, 6, 2)
 
+######################################################################
 
-
-
-PAREI: 32:43
-
-
-
-
-'''
 def soma(a, b):
     print(f'A = {a} e B = {b}')
     s = a + b
