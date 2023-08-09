7b6ee279d9933e2f0c8740ac6c3ea09ca3b4cdff
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-09 12:20:05 -0300

    Create aula21.py

---
+++ Python/pythonteste/aula21.py
@@ -0,0 +1,31 @@
+def par(n=0):
+    if n % 2 ==0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('Não é par!')
+
+
+
+'''  FATORIAL
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+num = int(input('Digite o número do fatorial: '))
+f4 = fatorial(num)
+print(f'Os resultados são {f1}, {f2}, {f3} e {f4}')
+
+'''
