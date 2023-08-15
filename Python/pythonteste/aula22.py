c98501f9761c0cd8ff1ee05b1925fa4e6c25a299
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-15 13:13:58 -0300

    Create aula22.py
    Aula 22

---
+++ Python/pythonteste/aula22.py
@@ -0,0 +1,11 @@
+def fatorial(n):
+    f = 1
+    for c in range(1, n+1):
+        f *= c
+    return f
+
+
+#Programa Principal
+num = int(input("Digite um valor: "))
+fat = fatorial(num)
+print(f"O fatorial de {num} é {fat}")
