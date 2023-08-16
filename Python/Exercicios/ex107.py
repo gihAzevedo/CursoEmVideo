2d49f33282fc8051e681205c1c7141b3c928cea9
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-16 07:56:59 -0300

    Update ex107.py
    Adicionei o enunciado.

--- Python/Exercicios/ex107.py
+++ Python/Exercicios/ex107.py
@@ -1,3 +1,6 @@
+'''Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade().
+Faça também um programa que importe esse módulo e use algumas dessas funções.'''
+
 from modulos import moeda
 
 p = float(input('Digite o preço: R$'))
