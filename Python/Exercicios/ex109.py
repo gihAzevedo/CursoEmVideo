5e6c1e836d038c8d52fbb48128133a7c0757f9d9
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-16 15:55:14 -0300

    não sei Ex 109
    Dar uma olhada depois

--- Python/Exercicios/ex109.py
+++ Python/Exercicios/ex109.py
@@ -1,5 +1,7 @@
-from ex109 import moeda
+''' Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o
+valor retornado por elas vai  ser ou não formatado pela função moeda(), desenvolvida no desafio 108.'''
 
+from ex109 import moeda
 
 p = float(input('Digite o preço: R$'))
 print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, False)}')
