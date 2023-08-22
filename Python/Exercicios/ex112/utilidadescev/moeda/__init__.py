60465844f8f94682a1f7443c3e20f996b9eac30f
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-22 07:22:22 -0300

    CreateEx112
    Resolução do Exercício 112 (validação de erros)

---
+++ Python/Exercicios/ex112/utilidadescev/moeda/__init__.py
@@ -0,0 +1,35 @@
+def moeda(preco=0, moeda='R$'):
+ return f'{moeda}{preco:.2f}'.replace('.', ',')
+
+def metade(preco, formatado = False):
+    res = preco / 2
+    return res if formatado is False else moeda(res)
+def dobro(preco, formatado = False):
+    res = preco * 2
+    return res if formatado is False else moeda(res)
+def aumentar(preco, porcentagem, formatado = False):
+    '''
+     -> Calcula o aumento de um determinado preço,
+    retornando o resultado com ou sem formatação.
+    :param preco: o preço que se quer reajustar
+    :param porcentagem: qual é a porcentagem do aumento
+    :param formatado: quer a saída formatada ou não?
+    :return: o valor reajustado, com ou sem formato.
+    '''
+    res = preco + (preco * porcentagem / 100)
+    return res if formatado is False else moeda(res)
+def diminuir(preco, porcentagem, formatado = False):
+    res = preco - (preco * porcentagem/ 100)
+    return res if formatado is False else moeda(res)
+
+def resumo(preco=0, taxaA=10, taxaR=5):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'{taxaA}% de aumento: \t{aumentar(preco, taxaA, True)}')
+    print(f'{taxaR}% de redução: \t{diminuir(preco, taxaR, True)}')
+    print('-' * 30)
+
