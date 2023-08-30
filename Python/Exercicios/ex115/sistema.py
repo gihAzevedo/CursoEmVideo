89a83e81d50eb996bc309413a5ba54ba9e0a6323
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-30 13:46:16 -0300

    Final Ex115
    Término da resolução do Exercício 115

--- Python/Exercicios/ex115/sistema.py
+++ Python/Exercicios/ex115/sistema.py
@@ -1,14 +1,27 @@
 '''Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
 O sistema só vai ter 2 opções: cadstrar uma nova pessoa e listar todas as pessoas cadastradas.'''
-from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 while True:
     resposta = menu(['Ver pessoas Cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opção 1')
+        #Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
-        cabecalho('Opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabecalho('Saindo do sistema... Até logo!')
         break
     else:
