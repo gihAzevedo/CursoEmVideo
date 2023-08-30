2a70697f36d2ec10f003fcc2f3788220bc72b8ac
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-30 09:08:17 -0300

    Enunciado

--- Python/Exercicios/ex115/sistema.py
+++ Python/Exercicios/ex115/sistema.py
@@ -1,3 +1,5 @@
+'''Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples.
+O sistema só vai ter 2 opções: cadstrar uma nova pessoa e listar todas as pessoas cadastradas.'''
 from lib.interface import *
 from time import sleep
 while True:
