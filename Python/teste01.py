43a39309258c469e8fdc084a1d2a0311e8c14ac4
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-08-31 07:43:04 -0300

    sei la

--- Python/teste01.py
+++ Python/teste01.py
@@ -1,5 +1,3 @@
-nome = input ('Qual é o seu nome? ')
-idade = input ('Qual é a sua idade? ')
-peso = input ('Qual é o seu peso? ')
-print(nome, idade, peso)
- 
+num = [3, 6, 4, 1]
+for n1, n2 in enumerate(num):
+    print(n1 + n2)
