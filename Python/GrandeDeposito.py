575a1dc8718999e101cb25b3efa1d36e69892997
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-09-06 07:26:39 -0300

    Resolução do exercicio

--- Python/GrandeDeposito.py
+++ Python/GrandeDeposito.py
@@ -1,8 +1,20 @@
+saldo = 0
 valor = float(input())
-
 if valor > 0:
-    //TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+    saldo+= valor
+    print("Deposito realizado com sucesso!")
+    print(f"Saldo atual: R$ {saldo:.2f}")
 elif valor == 0:
-   //TODO: Imprimir a mensagem de valor inválido.
+   print("Encerrando o programa...")
 else:
-  //TODO: Imprimir a mensagem de encerrar o programa.
+    while valor < 0:
+        print("Valor invalido! Digite um valor maior que zero.")
+        valor = float(input())
+        if valor > 0:
+            saldo += valor
+            print("Deposito realizado com sucesso!")
+            print(f"Saldo atual: R$ {saldo:.2f}")
+            break
+        elif valor == 0:
+            print("Encerrando o programa...")
+            break
