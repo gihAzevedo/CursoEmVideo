8bd053d33bdffc4756b627008e143f4058804962
Gideane Azevedo <137904765+gihAzevedo@users.noreply.github.com>
2023-10-23 15:21:22 -0300

    Update GrandeDeposito.py

--- Python/GrandeDeposito.py
+++ Python/GrandeDeposito.py
@@ -1,20 +1,16 @@
-saldo = 0
-valor = float(input())
-if valor > 0:
-    saldo+= valor
-    print("Deposito realizado com sucesso!")
-    print(f"Saldo atual: R$ {saldo:.2f}")
-elif valor == 0:
-   print("Encerrando o programa...")
-else:
-    while valor < 0:
-        print("Valor invalido! Digite um valor maior que zero.")
-        valor = float(input())
+saldo = 0.0
+while True:
+    try:
+        valor = float(input("Digite o valor do depósito (ou 0 para sair): "))
         if valor > 0:
             saldo += valor
-            print("Deposito realizado com sucesso!")
+            print("Depósito realizado com sucesso!")
             print(f"Saldo atual: R$ {saldo:.2f}")
-            break
-        elif valor == 0:
-            print("Encerrando o programa...")
-            break
+        else:
+            if valor == 0:
+                print("Encerrando o programa...")
+                break
+            else:
+                print("Valor inválido! Digite um valor maior que zero.")
+    except ValueError:
+        print("Valor inválido! Digite um valor numérico.")
